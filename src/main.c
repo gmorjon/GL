061f09fec3389e4e7ac6057b931da40703c672af
gmorjon <44802054+gmorjon@users.noreply.github.com>
2018-12-04 11:42:08 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
-int main(void)
+
+
+void main(void)
 {
-    return 0;
+	char prenom[100];
+	char nom[100];
+	char age[100];
+	
+	printf("veuillez taper un nom :");
+	scanf("%s", nom);
+
+	
+	printf("veuillez taper un prenom :");
+	scanf("%s", prenom);
+
+	
+	printf("veuillez taper un age :");
+	scanf("%s", age);
+
+	
+	printf("je m'appelle %s %s j'ai %s",prenom, nom, age);
+	getchar();
+
 }
